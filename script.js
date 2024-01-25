c54c9f1639a72b6610814b33d1ebe88e7ab5618d
DouglasPenn23 <63472155+DouglasPenn23@users.noreply.github.com>
2024-01-24 22:28:41 -0500

    More Script work

--- script.js
+++ script.js
@@ -2,3 +2,6 @@ document.getElementById('colorSlider').addEventListener('input', function() {
     const sliderValue = this.value;
     document.body.style.backgroundColor = getComputedStyle(document.documentElement).getPropertyValue(`--color-scheme-${sliderValue}`);
 });
+
+
+// Get a document element ID'd as color slider and add an event listener to the input then write a function
